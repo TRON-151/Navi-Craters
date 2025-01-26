7e9419d16dbf58e42b47c818ffd1a10356d539c2
Ousama Bin Zamir <71535064+TRON-151@users.noreply.github.com>
2025-01-26 01:45:26 +0100

    removed other obj files

--- graphics/objects/Objects.tsx
+++ graphics/objects/Objects.tsx
@@ -1,12 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="64" tileheight="64" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="Objects" tilewidth="64" tileheight="64" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <image source="D:/Forge-bound/Forge-bound/images/entities/player.png" width="8" height="15"/>
- </tile>
- <tile id="2">
-  <image source="D:/Forge-bound/Forge-bound/images/entities/enemy/idle/00.png" width="14" height="18"/>
- </tile>
  <tile id="4">
   <image source="../astronaut/idle/01.png" width="16" height="16"/>
  </tile>
